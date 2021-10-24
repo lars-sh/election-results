ac5a029c66d931c9a05d04aacadfb5fe2ba4d1e9
Lars Knickrehm <mail@lars-sh.de>
2021-10-24 22:50:35 +0200

    JaCoCo Maven Plugin: The Maven Plugin is referencing a moved Maven artifact.

--- mvn-suppressions.sh
+++ mvn-suppressions.sh
@@ -4,6 +4,12 @@ function suppressDuplicateProfileIdsInJfxParentPom() {
 	| grep --invert-match --perl-regexp "^\\[WARNING\\] Failed to build parent project for org\\.openjfx:javafx-(base|controls|fxml|graphics):jar:17\\.0\\.0\\.1$"
 }
 
+# JaCoCo Maven Plugin: The Maven Plugin is referencing a moved Maven artifact.
+function suppressJaCoCoMavenPlugin() {
+	cat < /dev/stdin \
+	| grep --invert-match --perl-regexp "^\\[WARNING\\] The artifact xml-apis:xml-apis:jar:2\\.0\\.2 has been relocated to xml-apis:xml-apis:jar:1\\.0\\.b2$"
+}
+
 # PMD: The Maven Plugin tries to perform Java type resolution on POM typed projects.
 function suppressPmdWarnings() {
 	cat < /dev/stdin \
@@ -12,4 +18,5 @@ function suppressPmdWarnings() {
 
 cat < /dev/stdin \
 | suppressDuplicateProfileIdsInJfxParentPom \
+| suppressJaCoCoMavenPlugin \
 | suppressPmdWarnings
